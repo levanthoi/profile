721c79b36026ab9515a88fc5285f80f3e518d943
levanthoi <lethoi1506@gmail.com>
2023-08-13 23:33:22 +0700

    update fix

--- src/app/(user)/project/page.tsx
+++ src/app/(user)/project/page.tsx
@@ -4,6 +4,10 @@ import React from 'react';
 
 async function getProjects() {
   const res = await getRepositories();
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
   return res.json();
 }
 
@@ -12,7 +16,9 @@ const page = async () => {
 
   return (
     <div className="my-4">
-      {projects?.map((project: any) => <ProjectItem key={project?.id} project={project} />)}
+      {projects?.length > 0
+        ? projects?.map((project: any) => <ProjectItem key={project?.id} project={project} />)
+        : 'Nothing'}
     </div>
   );
 };
